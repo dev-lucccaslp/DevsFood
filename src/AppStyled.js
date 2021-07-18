2a57b6924d2639810d20f85c5101808e131527f2
dev-lucccaslp <luccaslp99@gmail.com>
2021-07-18 18:46:55 -0300

    estrurando tamanhos da home page

---
+++ src/AppStyled.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Container = styled.div `
+    display: flex;
+    height: 100vh;
+`;
+
+export const Menu = styled.div `
+    display: flex;
+    background-color: #00ff00;
+    width: 80px;
+`;
+
+export const PageBody = styled.div `
+    display: flex;
+    flex:1;
+    background-color: #ff0000
+`;
